e25f00eb76910025af552b1aaa81295fc7513d53
Ashley Crawford <acrawford317@gmail.com>
2021-11-18 02:49:07 -0500

    ajax and json

--- setup.php
+++ setup.php
@@ -1,44 +1,30 @@
+
 <?php
 
 $lights_on = [];
 
-$rows = $_GET["rows-num"];
-$columns = $_GET["columns-num"];
-$length = $_GET["columns-num"] * $_GET["rows-num"];
+$rows = $_GET["rowsNum"];
+$columns = $_GET["columnsNum"];
+$total = $_GET["columnsNum"] * $_GET["rowsNum"];
 
-if($length>5){
-    for($i=0; $i<5; $i++){
-        $r = rand(0, rows);
-        $c = rand(0, columns);
-        $position = [$r, $c];
-        // $light_on.append( );
-    }
+// <!-- SOURCES USED:  https://stackoverflow.com/questions/17778723/generating-random-numbers-without-repeats -->
+
+function randomGen($min, $max, $quantity) {
+    $numbers = range($min, $max);
+    shuffle($numbers);
+    return array_slice($numbers, 0, $quantity);
 }
 
-$json = json_encode(lights_on);
+if($total<5){
+    $lights_on = randomGen(1, $total, $total);
+} else{
+    $lights_on = randomGen(1, $total, 5);
+}
 
+$lights["positions"]=$lights_on;
+$json = json_encode($lights);
+$t = gettype($json);
+
+echo $json;
 
 ?>
-
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">  
-        
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="author" content="Ashley Crawford">
-        <meta name="description" content="Lights Out Game - setup">  
-        <title>Lights Out Game Setup</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous"> 
-
-        <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
-    </head>
-    <body>
-
-    <?php
-        echo $length;
-    ?>
-
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
-    </body>
-</html>
